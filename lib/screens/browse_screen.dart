312d8ba21cb74f592616af253c2715b16a4d182d
Bishandeep13 <55798659+Bishandeep13@users.noreply.github.com>
2020-12-10 15:34:53 +0530

    Update browse_screen.dart

--- lib/screens/browse_screen.dart
+++ lib/screens/browse_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp3/screens/pdf_viewer.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class BrowseScreen extends StatefulWidget {
   static String id = 'browse_screen';
@@ -8,6 +9,16 @@ class BrowseScreen extends StatefulWidget {
 }
 
 class _BrowseScreenState extends State<BrowseScreen> {
+  Future<Widget> _getImage(BuildContext context, String imageName) async{
+            Image image;
+            await FireStorageService.loadImage(context, imageName).then((value){
+              image=Image.network(
+                value.toString(),
+                fit: BoxFit.scaleDown
+              );
+            });
+            return image;
+          }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,22 +27,41 @@ class _BrowseScreenState extends State<BrowseScreen> {
           itemCount: 10,
           itemBuilder: (context, index) {
             return Container(
-              margin: EdgeInsets.only(bottom: 19),
+              margin: EdgeInsets.only(bottom: 9),
               height: 81,
               width: MediaQuery.of(context).size.width,
-              color: Colors.black,
-              child: Row(
+              color: Colors.cyan[100],
+              child: Center(
+                child:Row(
                 children: <Widget>[
                   Container(
                     height: 81,
                     width: 62,
-                    child: Image(
-                      image: AssetImage('assets/images/b1.jpeg'),
-                      fit: BoxFit.fill,
-                    ),
+                    child: FutureBuilder(
+                      future: _getImage(context, "book$index.jpeg"),
+                      builder: (context, snapshot){
+                        if(snapshot.connectionState == ConnectionState.done){
+                          return Container(
+                            width: 62,
+                            height: 81,
+                            child: snapshot.data,
+                          );
+                        }
+
+                        if(snapshot.connectionState == ConnectionState.waiting){
+                          return Container(
+                            width: 62,
+                            height: 81,
+                            child: CircularProgressIndicator(),
+                          );
+                      }
+
+                      return Container();
+
+                      }),
                   ),
                   SizedBox(
-                    width: 40,
+                    width: 80,
                   ),
                   Container(
                     height: 40,
@@ -46,10 +76,18 @@ class _BrowseScreenState extends State<BrowseScreen> {
                   ),
                 ],
               ),
+              ),
             );
           },
         ),
       ),
     );
+  }
+}
+
+class FireStorageService extends ChangeNotifier{
+  FireStorageService();
+  static Future<dynamic> loadImage(BuildContext context, String Image) async{
+    return await FirebaseStorage.instance.ref().child(Image).getDownloadURL();
   }
 }
